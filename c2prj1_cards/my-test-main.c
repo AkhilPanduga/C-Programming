efd443a14b13c18ddfffeedb1293fcab207347c7
Coursera Learner <nobody@nowhere.nul>
2018-10-28 09:24:59 +0000

    Finished c2prj1_cards

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,10 @@
 #include "cards.h"
 
 int main(void) {
-
+  card_t a = card_from_num(38);
+  print_card(a);
+  char c = 'A';
+  char d = 's';
+  card_t b = card_from_letters(c, d);
+  print_card(b);
 }
