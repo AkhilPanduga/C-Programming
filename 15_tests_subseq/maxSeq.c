5407282cb73bf78294654d825d2720449ab6c96e
Coursera Learner <nobody@nowhere.nul>
2018-11-01 09:57:11 +0000

    Finished assignment 15

---
+++ 15_tests_subseq/maxSeq.c
@@ -0,0 +1,31 @@
+
+size_t maxSeq (int * array, size_t n) {
+  if (n == 0)
+    return 0;
+  int seq[n] = {0};
+  for (size_t i = 0; i < n; i++) {
+    seq[i] = *array;
+    array++;
+  }
+  if (n == 1)
+    return 1;
+  int max[n] = {0};
+  int * max_p = &max;
+  size_t x = 1;
+  for (size_t j = 1; j < n; j++) {
+    if (seq[j] > seq[j-1])
+      x++;
+    else {
+      *max_p = x;
+      max_p++;
+      x = 1;
+    }
+  }
+  int * seq_m = &max;
+  for (size_t k = 1; k < n; k++) {
+    if (*seq_m < max[k])
+      seq_m = &max[k];
+  }
+  x = *seq_m;
+  return x;
+}
---
+++ 15_tests_subseq/test-subseq.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n);
+
+int main (void) {
+  int array1[] = { };
+  int array2[] = { 1};
+  int array3[] = { -12,-9,-2,-1,1,4,6,7};
+  int array4[] = { 1,1,2,2,3,4,5,8};
+
+  if (maxSeq(array1, 0) != 0) {
+    return EXIT_FAILURE;
+  }
+  if (maxSeq(array2, 1) != 1) {
+    return EXIT_FAILURE;
+  }
+  if (maxSeq(array4, 8) != 5) {
+    return EXIT_FAILURE;
+  }
+  if (maxSeq(array3, 8) != 8) {
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
