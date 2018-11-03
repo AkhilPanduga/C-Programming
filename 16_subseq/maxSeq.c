d12ee7a33b33e8278373655574d97803b4db3cae
Coursera Learner <nobody@nowhere.nul>
2018-11-03 08:02:53 +0000

    Finished assignment 16

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -1,31 +1,24 @@
 
 size_t maxSeq (int * array, size_t n) {
   if (n == 0)
-    return 0;
+    return n;
+  size_t max = 1;
+  size_t l = 1;
   int seq[n] = {0};
   for (size_t i = 0; i < n; i++) {
     seq[i] = *array;
     array++;
   }
-  if (n == 1)
-    return 1;
-  int max[n] = {0};
-  int * max_p = &max;
-  size_t x = 1;
-  for (size_t j = 1; j < n; j++) {
+  for (size_t j = 1; j <= n; i++) {
     if (seq[j] > seq[j-1])
-      x++;
-    else {
-      *max_p = x;
-      max_p++;
-      x = 1;
-    }
+      l++;
+    else
+      if (max < l) {
+	max = l;
+	l = 1;
+      }
+    if (max < l)
+      max = l;
   }
-  int * seq_m = &max;
-  for (size_t k = 1; k < n; k++) {
-    if (*seq_m < max[k])
-      seq_m = &max[k];
-  }
-  x = *seq_m;
-  return x;
+  return max;
 }
---
+++ 16_subseq/test-subseq.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n);
+
+int main (void) {
+  int array1[] = { 1};
+  int array2[] = { 1};
+  int array3[] = { -12, -9, -2, -1, 1, 4, 6, 7};
+  int array4[] = { 1, 1, 2, 2, 3, 4, 5, 8};
+
+  if (maxSeq(array1, 0) != 0) {
+    return EXIT_FAILURE;
+  }
+  if (maxSeq(array2, 1) != 1) {
+    return EXIT_FAILURE;
+  }
+  if (maxSeq(array4, 10) != 5) {
+    return EXIT_FAILURE;
+  }
+  if (maxSeq(array4, 8) != 5) {
+    return EXIT_FAILURE;
+  }
+  if (maxSeq(array3, 8) != 8) {
+    return EXIT_FAILURE;
+  }
+  printf("YES\n");
+  return EXIT_SUCCESS;
+}
