4a5f69e9d1805bdfb5bc7b862dec5734c4b41be3
Coursera Learner <nobody@nowhere.nul>
2018-11-03 16:21:04 +0000

    Finished assignment 18

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -4,6 +4,22 @@
 
 void reverse(char * str) {
   //WRITE ME!
+  int n = 0;
+  for (int i = 0; str[i] != '\0'; i++)
+    n++;
+  char temp[n];
+  if (n != 0) {
+    str += (n - 1);
+    for (int j = 0; j < n; j++) {
+      temp[j] = *str;
+      if (j < (n - 1))
+	str--;
+    }
+    for (int k = 0; k < n; k++) {
+      *str = temp [k];
+      str++;
+    }
+  }
 }
 
 int main(void) {
