a8dea2a86c85bcddcc1b12e5aaf9192fbf478366
Coursera Learner <nobody@nowhere.nul>
2018-11-08 05:38:07 +0000

    Finished c3prj1_deck

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -3,17 +3,51 @@
 #include <assert.h>
 #include "deck.h"
 void print_hand(deck_t * hand){
-
+  deck_t h = *hand;
+  for (size_t i = 0; i < hand->n_cards; i++) {
+    print_card(*(h.cards)[i]);
+    printf(" ");
+  }
 }
 
 int deck_contains(deck_t * d, card_t c) {
+  deck_t d_c = *d;
+  card_t s;
+  for (size_t i = 0; i < d->n_cards; i++) {
+    s = *(d_c.cards)[i];
+    if (s.value == c.value) {
+      if (s.suit == c.suit) {
+	return 1;
+      }
+    }
+  }
   return 0;
 }
 
 void shuffle(deck_t * d){
-
+  deck_t d_c = *d;
+  card_t s;
+  for (size_t k = 0; k < d->n_cards; k++) {
+    size_t j = k + (random()%(d->n_cards - k));
+    s = *(d_c.cards)[k];
+    *(d_c.cards)[k] = *(d_c.cards)[j];
+    *(d_c.cards)[j] = s;
+  }
 }
 
 void assert_full_deck(deck_t * d) {
-
+  deck_t d_c = *d;
+  card_t s, r, temp;
+  for (size_t k = 0; k < (d->n_cards); k++) {
+    temp = *(d_c.cards)[k];
+    assert_card_valid(temp);
+  }
+  for (size_t i = 0; i < (d->n_cards) - 1; i++) {
+    s = *(d_c.cards)[i];
+    for (size_t j = i + 1; j < (d->n_cards); j++) {
+    r = *(d_c.cards)[j];
+    assert(((s.value != r.value) && (s.suit != r.suit)) || ((s.value == r.value) && (s.suit != r.suit)) || ((s.value != r.value) && (s.suit == r.suit)));
+    }
+  }
 }
+
