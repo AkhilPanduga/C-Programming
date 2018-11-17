52ae28d98a51d8639082128821c7436267e3ec46
Coursera Learner <nobody@nowhere.nul>
2018-11-17 09:31:52 +0000

    Finished assignment 25

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -12,20 +12,16 @@ int maxfreq (int * arr) {
   return maxf;
 }
 
-void decrypt (FILE * g, int n) {
+void decrypt (FILE * f) {
   int c;
-  int i = 0;
-  int ch[n];
-  while ((c = fgetc(g)) != EOF) {
-    if(isalpha(c)) {
-      ch[i] = tolower(c);
-      i++;
-    }
-  }
   int freq[26] = {0};
   int key;
-  for (int j = 0; j < n; j++)
-    freq[ch[j] - 97] += 1;
+  while ((c = fgetc(g)) != EOF) {
+    if(isalpha(c)) {
+      c = tolower(c);
+      freq[c - 97] += 1;
+    }
+  }
   int max = maxfreq(freq);
   if (max >= 4)
     key = max - 4;
@@ -35,38 +31,18 @@ void decrypt (FILE * g, int n) {
   else printf("%d\\n", key);
 }
 
-int lettercount (FILE * f) {
-  int c;
-  int l = 0;
-  while ((c = fgetc(f)) != EOF) {
-    if(isalpha(c)) {
-      l++;
-    }
-  }
-  return l;
-}
-
 int main (int argc, char ** argv) {
   if (argc != 2) {
     fprintf(stderr,"Enter Input File\n");
     return EXIT_FAILURE;
   }
-  int n = 0;
   FILE * f = fopen(argv[1], "r");
   if (f == NULL) {
     perror("Error Opening File");
     return EXIT_FAILURE;
   }
-  n = lettercount(f);
-  fclose(f);
+  decrypt(f);
   if (fclose(f) != 0) {
-    perror("Failed to close the input file");
-    return EXIT_FAILURE;
-  }
-  FILE * g = fopen(argv[1], "r");
-  decrypt(g, n);
-  fclose(g);
-  if (fclose(g) != 0) {
     perror("Failed to close the input file");
     return EXIT_FAILURE;
   }
