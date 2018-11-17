4adf7dd1370a5ad0d88797baf51aa5e87176157f
Coursera Learner <nobody@nowhere.nul>
2018-11-17 00:34:36 +0000

    Finished c3prj2_eval

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -83,6 +83,7 @@ size_t  find_secondary_pair(deck_t * hand,
 
 int is_n_length_straight_at(deck_t * hand, size_t index, suit_t fs, int n) {
   size_t count = 0;
+  int diff = 1;
   card_t s, r;
   deck_t h_c = *hand;
   size_t idx = index;
@@ -91,14 +92,29 @@ int is_n_length_straight_at(deck_t * hand, size_t index, suit_t fs, int n) {
     r = *(h_c.cards)[idx];
     if (fs != NUM_SUITS) {
       if (r.suit == fs) {
-	if (((r.value - s.value) < 2) && (s.suit == fs)) {
-	  if ((r.value - s.value) == 0)
-	    idx = i;
-	  if ((r.value - s.value) == 1) {
-	    idx = i;
-	    count++;
-	    if (count == n-1)
-	      return 1;
+	if ((r.value - s.value) < 2) {
+	  if (s.suit == fs) {
+	    if ((r.value - s.value) == 0)
+	      idx = i;
+	    if ((r.value - s.value) == 1) {
+	      idx = i;
+	      count++;
+	      if (count == n-1)
+		return 1;
+	    }
+	  }
+	  else {
+	    for (size_t j = i + 1; j < (hand->n_cards); j++) {
+	      s = *(h_c.cards)[j];
+	      if ((r.value - s.value) <= diff) {
+		if(s.suit == fs) {
+		  count++;
+		  diff++;
+		  if (count == n-1)
+		    return 1;
+		}
+	      }
+	    }
 	  }
 	}
 	else return 0;
