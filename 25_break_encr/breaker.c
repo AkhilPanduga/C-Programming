a09f607b9fe10a17826ff699b574106a460229be
Coursera Learner <nobody@nowhere.nul>
2018-11-17 08:02:40 +0000

    Finished assignment 25

---
+++ 25_break_encr/breaker.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int maxfreq (int * arr) {
+  int max = 0;
+  for (int i = 0; i < 26; i++) {
+    if (arr[i] > arr[max])
+      max = i;
+  }
+  return max;
+}
+
+void decrypt (int * arr) {
+  int freq[26] = {0};
+  int key;
+  for (int i = 0; j < sizeof(arr)/sizeof(int); i++)
+    freq[arr[i] - 97] += 1;
+  max = maxfreq(freq);
+  if (max >= 4)
+    key = max - 4;
+  else key = max + 22;
+  if (key < 0 || key >= 26)
+    fprintf(stderr, "Decyption Failed");
+  else printf("%d\\n", key);
+}
+
+int main (int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr,"Enter Input File\n");
+    return EXIT_FAILURE;
+  }
+  int c;
+  int ch[];
+  while ((c = fgetc(f)) != EOF) {
+    int i = 0;
+    if(isalpha(c)) {
+      ch[i] = tolower(c);
+      i++;
+    }
+  }
+  FILE * f = fopen(argv[2], "r");
+  if (f == NULL) {
+    perror("Error Opening File");
+    return EXIT_FAILURE;
+  }
+  decrypt(ch);
+  if (fclose(f) != 0) {
+    perror("Failed to close the input file");
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
