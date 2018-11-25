9d5532a30d8b5328e54da703f10336a3a0cac11e
Coursera Learner <nobody@nowhere.nul>
2018-11-25 03:58:27 +0000

    Finished assignment 30

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,39 +16,46 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+int  sortlines(FILE * f) {
+  char ** lines = NULL;
+  char * in = NULL;
+  size_t s = 0;
+  size_t j = 0;
+  while(getline(&in, &s, f) >= 0) {
+    lines = realloc(lines, (j + 1) * sizeof(*lines));
+    lines[j] = in;
+    in = NULL;
+    j++;
+  }
+  free(in);
+  if (j == 0) {
+    fprintf(stderr, "Invalid no. of Lines");
+    for (size_t l = 0; l < j; l++)
+      free(lines[l]);
+    free(lines);
+    return -1;
+  }
+  sortData(lines, j);
+  for (size_t k = 0; k < j; k++) {
+    printf("%s", lines[k]);
+    free(lines[k]);
+  }
+  free(lines);
+  return 0;
+}
+
 int main(int argc, char ** argv) {
   
   //WRITE YOUR CODE HERE!
   if (argc == 1) {
     FILE * f = stdin;
     if (f == NULL) {
-	perror("Cannot open file");
-	return EXIT_FAILURE;
-      }
-    char ** lines = NULL;
-    char * in = NULL;
-    size_t s = 0;
-    size_t j = 0;
-    while(getline(&in, &s, f) >= 0) {
-      lines = realloc(lines, (j + 1) * sizeof(*lines));
-      lines[j] = in;
-      in = NULL;
-      j++;
-    }
-    free(in);
-    if (j == 0) {
-      fprintf(stderr, "Invalid no. of Lines");
-      for (size_t l = 0; l < j; l++)
-	free(lines[l]);
-      free(lines);
+      perror("Cannot open file");
       return EXIT_FAILURE;
     }
-    sortData(lines, j);
-    for (size_t k = 0; k < j; k++) {
-      printf("%s", lines[k]);
-      free(lines[k]);
-    }
-    free(lines);
+    int r = sortlines(f);
+    if (r < 0)
+      return EXIT_FAILURE;
     if (fclose(f) != 0) {
       perror("Failed to close the file");
       return EXIT_FAILURE;
@@ -61,30 +68,9 @@ int main(int argc, char ** argv) {
 	perror("Cannot open file");
 	return EXIT_FAILURE;
       }
-      char ** lines = NULL;
-      char * in = NULL;
-      size_t s = 0;
-      size_t j = 0;
-      while(getline(&in, &s, f) >= 0) {
-	lines = realloc(lines, (j + 1) * sizeof(*lines));
-	lines[j] = in;
-	in = NULL;
-	j++;
-      }
-      free(in);
-      if (j == 0) {
-	fprintf(stderr, "Invalid no. of Lines");
-	for (size_t l = 0; l < j; l++)
-	  free(lines[l]);
-	free(lines);
+      int r = sortlines(f);
+      if (r < 0)
 	return EXIT_FAILURE;
-      }
-      sortData(lines, j);
-      for (size_t k = 0; k < j; k++) {
-	printf("%s", lines[k]);
-	free(lines[k]);
-      }
-      free(lines);
       if (fclose(f) != 0) {
 	perror("Failed to close the file");
 	return EXIT_FAILURE;
